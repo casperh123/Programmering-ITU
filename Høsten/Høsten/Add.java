bb3eda5261baff229763e0ef2e485ebd1155567e
Casper Holten <69851314+casperh123@users.noreply.github.com>
2022-08-31 00:20:19 +0200

    asd

---
+++ Høsten/Høsten/Add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Add {
+    public static void main(String args[]) {
+
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println(a * b);
+    }
+}
---
+++ Lektion 1/Opgaver/HelloWorld.java
@@ -0,0 +1,49 @@
+class HelloWorld {
+    static void hello() {
+        System.out.println("Hello World");
+    }
+    
+    static void heltal() {
+        
+        int current_year = 2022;
+        
+        System.out.println(current_year);
+    }
+    
+    static void tekststreng() {
+        
+        String my_name_greeting = "Hej, mit navn er Casper!";
+        
+        System.out.println(my_name_greeting);
+    }
+    
+    static void kommatal() {
+        
+        double absolutte_nulpunkt = -273.15;
+        
+        System.out.println(absolutte_nulpunkt);
+    }
+    
+    static void inkrementering() {
+        int age = 21;
+        
+        System.out.println("Jeg er " + age + " år gammel");
+        
+        age++;
+        
+        System.out.println("Næste år er jeg " + age + " år gammel");
+    }
+    
+    static void jamesBond() {
+        String first_name = "James";
+        String last_name = "Bond";
+        
+        System.out.println("My name is " + last_name + ", " + first_name + " " + last_name);
+        
+        first_name = "Casper";
+        last_name = "Holten";
+        
+        System.out.println("My name is " + last_name + ", " + first_name + " " + last_name);
+
+    }
+}
