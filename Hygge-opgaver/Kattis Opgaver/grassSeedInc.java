bf5eebd0c5b1cd57ca5165bee601522937f28db9
Casper Holten <69851314+casperh123@users.noreply.github.com>
2022-09-01 17:10:46 +0200

    asd

---
+++ Hygge-opgaver/Kattis Opgaver/grassSeedInc.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class grassSeedInc {
+    public static void main(String args[]) {
+
+        Scanner input = new Scanner(System.in);
+
+        double sowCost = input.nextDouble();
+        int lawnsToSow = input.nextInt();
+        double totalSowCost = 0;
+
+        for (int i = 0; i < lawnsToSow; i++) {
+
+            double lawnWidth = input.nextDouble();
+            double lawnLength = input.nextDouble();
+
+            totalSowCost += lawnLength * lawnWidth * sowCost;
+        }
+
+        System.out.println(totalSowCost);
+
+    }
+}
---
+++ Hygge-opgaver/Kattis Opgaver/greetings.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class greetings {
+    public static void main(String args[]) {
+
+        Scanner input = new Scanner(System.in);
+
+
+        String initialGreeting = input.nextLine();
+        String modifiedGreeting = "";
+        int modifiedGreetingLength = (initialGreeting.length() - 2) * 2;
+
+        for (int i = 0; i < modifiedGreetingLength; i++) {
+            modifiedGreeting += "e";
+        }
+
+        System.out.println("h" + modifiedGreeting + "y");
+    }
+}
