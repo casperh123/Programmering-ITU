f560e3e907968b89e917fc865eb7ed25bb5b9956
Casper Holten <69851314+casperh123@users.noreply.github.com>
2022-09-16 10:07:26 +0200

    asd

--- Grundllaeggende-programmering/Lektion-6/Hashmaps-hashsets/HashSetTest.java
+++ Grundllaeggende-programmering/Lektion-6/Hashmaps-hashsets/HashSetTest.java
@@ -19,7 +19,7 @@ public class HashSetTest {
     
     }
     
-    static void hej() {
+    static void uniqueTester() {
         
         Scanner input = new Scanner(System.in);
         
@@ -34,5 +34,99 @@ public class HashSetTest {
         
         System.out.println("Unikke tal i listen: " + unique(liste));
     
+    }
+    
+    public static int uniqueNames(ArrayList<String> nameList) {
+        
+        HashSet<String> uniqueNames = new HashSet<>();
+        
+        for(String name : nameList) {
+            uniqueNames.add(name);
+        }
+        
+        return uniqueNames.size();
+    }
+    
+    public static void uniqueNamesTester() {
+        
+        Scanner input = new Scanner(System.in);
+        int numberInputs = input.nextInt();
+        ArrayList<String> names = new ArrayList<>();
+        
+        for (int i = 0; i < numberInputs; i++) {
+            names.add(input.next());
+        }
+        
+        int unikkeNavne = uniqueNames(names);
+        
+        System.out.println(unikkeNavne + " unikke navne.");
+        
+    }
+    
+    public static int uniqueNameslengths(ArrayList<String> namesList) {
+        
+        HashSet<Integer> nameLengths = new HashSet<>();
+        
+        for (String name : namesList) {
+            
+            nameLengths.add(name.length());
+        
+        }
+        
+        return nameLengths.size();
+        
+    }
+    
+    public static void uniqueNamesLengthsTester() {
+        
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> nameList = new ArrayList<>();
+        
+        int testCases = input.nextInt();
+        
+        for(int i = 0; i < testCases; i++) {
+            nameList.add(input.next());
+        }
+        
+        System.out.println(uniqueNameslengths(nameList));
+        
+    }
+    
+    public static void nameLengths(ArrayList<String> nameList) {
+        
+        HashMap<Integer, Integer> nameLengthOccurence = new HashMap<>();
+        
+        for(String name : nameList) {
+            
+            int nameLength = name.length();
+            int indexNumber = 0;
+            
+            if(nameLengthOccurence.containsKey(nameLength)) {
+                
+                indexNumber = nameLengthOccurence.get(nameLength);
+                
+            }
+            
+            nameLengthOccurence.put(nameLength, indexNumber + 1);
+            
+        }
+        
+        System.out.println(nameLengthOccurence.keySet());
+        System.out.println(nameLengthOccurence.values());
+    }
+    
+    public static void nameLengthOccurenceTester() {
+        
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> nameList = new ArrayList<>();
+        
+        int testCases = input.nextInt();
+        
+        for(int i = 0; i < testCases; i++) {
+            nameList.add(input.next());
+        }
+        
+        nameLengths(nameList);
+        
     }
 }
