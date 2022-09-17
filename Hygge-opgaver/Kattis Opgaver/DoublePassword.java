298e578d9110a17595dddebf07ade3a54cee7af7
Casper Holten <69851314+casperh123@users.noreply.github.com>
2022-09-17 14:47:36 +0200

    asd

--- Hygge-opgaver/Kattis Opgaver/DoublePassword.java
+++ Hygge-opgaver/Kattis Opgaver/DoublePassword.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
-import java.util.HashSet;
 
 public class DoublePassword {
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
 
-        String firstString = input.nextLine();
-        String secondString = input.nextLine();
-        String[] arrayFirstString = firstString.split("");
-        String[] arraySecondString = secondString.split("");
+        String[] arrayFirstString = input.nextLine().split("");
+        String[] arraySecondString = input.nextLine().split("");
         int differences = 0;
         int totalCombinations = 0;
 
@@ -21,11 +18,7 @@ public class DoublePassword {
 
         }
 
-        if(differences == 0) {
-            totalCombinations = 1;
-        } else {
-            totalCombinations = (int)Math.round(Math.pow(differences, differences));
-        }
+        totalCombinations = (int)Math.pow(2, differences);
 
         System.out.println(totalCombinations);
 
