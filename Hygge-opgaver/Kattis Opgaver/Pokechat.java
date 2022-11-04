194e27e3ebac74f52d3f518e1a50079cf42bd7c6
Casper Holten <69851314+casperh123@users.noreply.github.com>
2022-11-04 14:21:30 +0100

    asd

---
+++ Hygge-opgaver/Kattis Opgaver/Pokechat.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Pokechat {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        String[] inputString = input.nextLine().split("");
+        String[] decodingString = input.nextLine().split("");
+
+        String decodedString = "";
+
+        for(int index = 0; index < decodingString.length - 2; index += 3) {
+            
+            int calculatedIndex = 0;
+
+            for(int i = index; i < index+2 && i < decodingString.length; i++) {
+            
+                int indexInteger = Integer.parseInt(decodingString[i]);
+    
+                if(i % 3 == 0) {
+                    calculatedIndex += indexInteger * 100;
+                } else if (i % 3 == 1) {
+                    calculatedIndex += indexInteger * 10;
+                } else {
+                    calculatedIndex += indexInteger;
+                }
+            }
+
+            decodedString += inputString[calculatedIndex];
+
+        }
+
+        System.out.println(decodedString);
+    }
+}
