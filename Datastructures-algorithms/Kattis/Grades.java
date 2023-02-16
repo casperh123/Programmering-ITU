ab3356df4a7e01eedb6e75fea66d83030ef49d56
Casper Holten <69851314+casperh123@users.noreply.github.com>
2023-02-16 09:17:11 +0100

    asd

---
+++ Datastructures-algorithms/Kattis/Grades.java
@@ -0,0 +1,22 @@
+import java.util.Map;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Grades {
+    public static void main(String[] args) {
+
+        int iterations = StdIn.readInt();
+        Map<String, String> studentGrade = new HashMap<>();
+
+        for (int i = 0; i < iterations; i++) {
+            
+            String[] inputString = StdIn.readLine().split(" ");
+
+
+
+
+        }
+    }
+}
+
