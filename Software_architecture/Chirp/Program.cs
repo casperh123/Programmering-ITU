ab4a118b3a45bc9e5651847169009374a53f079a
Casper Holten <casho@itu.dk>
2023-08-31 12:58:54 +0200

    asd

---
+++ Software_architecture/Chirp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+if (args[0].Equals("say"))
+{
+    string message = args[1];
+    int frequency = int.Parse(args[2]);
+
+    for (int i = 0; i < frequency; i++)
+    {
+        Console.WriteLine(message + " ");
+    }
+}
