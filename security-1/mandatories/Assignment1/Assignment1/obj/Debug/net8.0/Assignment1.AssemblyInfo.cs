b02128b7af67a337659c2e1b441ee52e39dbd703
casho <casho@itu.dk>
2024-10-01 15:21:47 +0200

    Security stuff

--- security-1/mandatories/Assignment1/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ security-1/mandatories/Assignment1/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c94e3f5a419d2b6143033eb1884dd1b67ffe08cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6f88add4fb01e4c4adefa1d296ab70838d49009")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
